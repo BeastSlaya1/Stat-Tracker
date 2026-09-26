79a53b16cbb538b054f89763b1e5a07d38a60989
BeastSlaya1 <joshuapieterse1@gmail.com>
2026-09-26 08:07:03 +0200

    Delete all files

--- packages/stc_camera_preview/stc_camera_preview/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Public exports for the stc-camera-preview package.
-"""
-
-from stc_camera_preview.camera_preview import StcCameraPreview
-
-from stc_camera_preview.rtc_video import StcRtcVideo
-
-__all__ = ["StcCameraPreview", "StcRtcVideo"]
